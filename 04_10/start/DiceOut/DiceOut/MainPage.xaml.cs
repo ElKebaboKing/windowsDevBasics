7ebbd287875456d0d4d24313c0e9ccaa60bf63dc
Doug Winnie <doug@sfdesignerdw.com>
2016-05-12 15:07:27 -0700

    Initial Exercise File Commit - Pre-Release Version
    This is the initial release of the exercise files for "Learn Universal
    Windows App Development: The Basics" at completion of recording.

---
+++ 04_10/start/DiceOut/DiceOut/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DiceOut
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public int[] Dice = new int[3];
+
+        public Random RandomGenerator = new Random();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void RollButton_Click(object sender, RoutedEventArgs e)
+        {
+            // DieValueText.Text = "Button clicked!";
+
+            for (int i = 0; i < Dice.Length; i++)
+            {
+                int DieValue = RandomGenerator.Next(1, 7);
+                Dice[i] = DieValue;
+            }
+
+            DieValueText.Text = $"The values are {Dice[0]}, {Dice[1]}, and {Dice[2]}.";
+
+            ApplyGameRules();
+        }
+
+        private void ApplyGameRules()
+        {
+            // If the first die is equal to the second
+            // AND
+            // If the first die is euqal to the third
+            // then we have a triple!
+
+            if (Dice[0] == Dice[1] && Dice[0] == Dice[2])
+            {
+                // Triple
+            } else if (Dice[0] == Dice[1] || Dice[1] == Dice[2] || Dice[0] == Dice[2])
+            {
+                // Double
+            } else
+            {
+                // No matches
+            }
+        }
+    }
+}
---
+++ 04_11/finish/DiceOut/DiceOut/MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DiceOut
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public int[] Dice = new int[3];
+        public int Score = 0;
+
+        public Random RandomGenerator = new Random();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SetupGame();
+        }
+
+        private void SetupGame()
+        {
+            WinText.Text = "Click the \"Roll Die\" button to play";
+            ScoreText.Text = $"Score: {Score}";
+        }
+
+        private void RollButton_Click(object sender, RoutedEventArgs e)
+        {
+            // DieValueText.Text = "Button clicked!";
+
+            for (int i = 0; i < Dice.Length; i++)
+            {
+                int DieValue = RandomGenerator.Next(1, 7);
+                Dice[i] = DieValue;
+            }
+
+            DieValueText.Text = $"The values are {Dice[0]}, {Dice[1]}, and {Dice[2]}.";
+
+            ApplyGameRules();
+        }
+
+        private void ApplyGameRules()
+        {
+            int NewScore = 0;
+            string WinMessage = "";
+
+            if (Dice[0] == Dice[1] && Dice[0] == Dice[2])
+            {
+                // Triple
+                if (Dice[0] < 6)
+                {
+                    NewScore = Dice[0] * 100;
+                    WinMessage = $"You rolled a {Dice[0]} for {NewScore} points.";
+                }
+                else
+                {
+                    NewScore = 1000;
+                    WinMessage = "Triple 6's! You scored 1,000 points!";
+                }
+
+
+            } else if (Dice[0] == Dice[1] || Dice[1] == Dice[2] || Dice[0] == Dice[2])
+            {
+                // Double
+                NewScore = 50;
+                WinMessage = "You scored doubles for 50 points.";
+
+            } else
+            {
+                // No matches
+                NewScore = 0;
+                WinMessage = "No matches this roll.";
+            }
+
+            Score = Score + NewScore;
+            ScoreText.Text = $"Score: {Score}";
+            WinText.Text = WinMessage;
+        }
+    }
+}
